c090fc5cc16d46d43d95ec32a836a857fbde1e5b
Dave-MK <davidmkilgallon@gmail.com>
2025-05-06 02:38:21 +0100

    Add: Initial styling

---
+++ constants.ts
@@ -0,0 +1,9 @@
+export const GAME_WORD_LENGTH = 5;
+
+export const GAME_ROUNDS = 6;
+
+export const ENTER = "Enter";
+
+export const BACKSPACE = "Backspace";
+
+export type LetterState = "default" | "incorrect" | "correct" | "out-of-place";
