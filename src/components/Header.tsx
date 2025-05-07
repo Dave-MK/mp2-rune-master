647e0364ae6204151b18dedc658e1cf7ace2720c
Dave-MK <davidmkilgallon@gmail.com>
2025-05-07 11:10:10 +0100

    Fix: functional issues | Add: style and icons to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,23 @@
+import { FaRobot } from "react-icons/fa"
+import { GiHamburgerMenu } from "react-icons/gi"
+
 type Props = {}
 
 export const Header = (_props: Props) => {
     return (
         <>
-            <div className="flex justify-center items-center w-full h-12 border-b border-gray-600">
+            <div className="flex justify-between items-center w-full h-14 border-b border-gray-600 p-8">
+                <span className="text-white text-5xl">
+                    <FaRobot />
+                </span>
                 <h1>
                     THAT'S THE WORD!
                 </h1>
+                <span className="text-white text-4xl">
+                    <GiHamburgerMenu />
+                </span>
+                
+                
             </div>
         </>
     )
