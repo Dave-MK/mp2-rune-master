c02e6a360baf89e552360dd5e2e38898fc16f469
Dave-MK <davidmkilgallon@gmail.com>
2025-05-23 01:10:39 +0100

    Fix: some broken image links in deployed version

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,14 +6,15 @@ export const Footer = (_props: Props) => {
     return (
         <>
             <div className="w-[100vw] grid mt-auto mb-auto justify-center items-center">
-                {/* <a href="https://github.com/dave-mk/mp2-rune-master"> */}
+                
                     <div className="col-start-1 row-start-1">
-                        <DiGithubFull className="text-6xl md:text-7xl lg:text-8xl text-teal-600"/>
+                        <a href="https://github.com/dave-mk/mp2-rune-master">
+                            <DiGithubFull className="text-6xl md:text-7xl lg:text-8xl text-teal-600"/>
+                        </a>
                     </div>
                     <div className="col-start-1 row-start-1">
                         <DiGithubFull className="text-6xl md:text-7xl lg:text-8xl text-teal-600 animate-ping"/>
                     </div>  
-                {/* </a> */}
             </div>
         </>
     )
