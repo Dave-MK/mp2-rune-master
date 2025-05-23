bd8f5c1ac9bfbaf70a1a2d65b1af1d906296768a
Dave-MK <davidmkilgallon@gmail.com>
2025-05-23 03:41:45 +0100

    Fix: Broken github link in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,15 +6,22 @@ export const Footer = (_props: Props) => {
     return (
         <>
             <div className="w-[100vw] grid mt-auto mb-auto justify-center items-center">
-                
-                    <div className="col-start-1 row-start-1">
-                        <a href="https://github.com/dave-mk/mp2-rune-master">
-                            <DiGithubFull className="text-6xl md:text-7xl lg:text-8xl text-teal-600"/>
-                        </a>
-                    </div>
-                    <div className="col-start-1 row-start-1">
-                        <DiGithubFull className="text-6xl md:text-7xl lg:text-8xl text-teal-600 animate-ping"/>
-                    </div>  
+                    <a
+                        href="https://github.com/Dave-MK/mp2-rune-master"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="col-start-1 row-start-1"
+                    >
+                        <DiGithubFull className="text-6xl md:text-7xl lg:text-8xl text-teal-600" />
+                    </a>
+                    <a
+                        href="https://github.com/Dave-MK/mp2-rune-master"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="col-start-1 row-start-1"
+                    >
+                        <DiGithubFull className="text-6xl md:text-7xl lg:text-8xl text-teal-600 animate-ping" />
+                    </a>
             </div>
         </>
     )
